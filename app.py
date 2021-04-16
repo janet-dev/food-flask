a2d82455059ebb070fc1dae0cdbf42d085c59815
JD <67335440+janet-dev@users.noreply.github.com>
2021-04-16 11:49:24 +0100

    Updated files

--- app.py
+++ app.py
@@ -50,8 +50,9 @@ Its name is a play on the phrase "Python data analysis".
 from flask import Flask, request, render_template
 import pandas as pd
 import requests
-import webbrowser
+# import webbrowser
 # The webbrowser module provides a high-level interface to allow displaying Web-based documents to users.
+
 
 app = Flask(__name__)
 # clear the cache
@@ -63,14 +64,16 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 def root_page():
     ingredient = ''     # user's ingredient for searching on
     max_time = ''       # user's cooking & prep time limit
-    cuisine = ''
-    if request.method == 'POST' and ('userfood' and 'usertime' and 'usercuisine') in request.form:
+    cuisine = ''        # cuisine type
+    health = ''         # health label
+    if request.method == 'POST' and ('userfood' and 'usertime' and 'usercuisine' and 'userhealth') in request.form:
         ingredient = request.form.get('userfood')       # ingredient
         max_time = int(request.form.get('usertime'))    # max cooking time was a string
-        cuisine = request.form.get('usercuisine')
-        recipe_search(ingredient, max_time, cuisine)
-        run_csv(ingredient, max_time, cuisine)
-    return render_template("index.html", ingredient=ingredient, max_time=max_time, cuisine=cuisine)
+        cuisine = request.form.get('usercuisine')       # cuisine type (e.g. Chinese, Eastern European)
+        health = request.form.get('userhealth')         # health label (e.g. vegan, gluten-free)
+        recipe_search(ingredient, max_time, cuisine, health)
+        run_csv(ingredient, max_time, cuisine, health)
+    return render_template("index.html", ingredient=ingredient, max_time=max_time, cuisine=cuisine, health=health)
 
 
 @app.route('/recipes', methods=['GET', 'POST'])     # Flask renders pandas' html table
@@ -78,26 +81,34 @@ def recipes_html():
     return render_template('recipes.html')
 
 
-def recipe_search(ingredient, max_time, cuisine):
+def recipe_search(ingredient, max_time, cuisine, health):
     app_id = 'YOUR_ID'
     app_key = 'YOUR_KEY'
-    result = requests.get(
-    'https://api.edamam.com/search?q={}&app_id={}&app_key={}&time=1-{}&cuisineType={}'.format(ingredient, app_id, app_key, max_time, cuisine))
+    # get your own id & key, free of charge, from https://developer.edamam.com/
+    if health == 'none':  # for health result 'none', do not add health as a search term for API - Alex
+        result = requests.get(
+        'https://api.edamam.com/search?q={}&app_id={}&app_key={}&time=1-{}&cuisineType={}'.format(ingredient, app_id, app_key, max_time, cuisine))
+    else:
+        result = requests.get('https://api.edamam.com/search?q={}&app_id={}&app_key={}&time=1-{}&cuisineType={}&health={}'.format(ingredient, app_id, app_key, max_time, cuisine, health))
     data = result.json()
     return data['hits']
 
 
 # Alex's pandas
-def run_csv(ingredient, max_time, cuisine):
+def run_csv(ingredient, max_time, cuisine, health):
     d = []  # clear the dataset
-    # ingredient = input('Enter an ingredient: ')  # Commented out by Janet
-    results = recipe_search(ingredient, max_time, cuisine)
+    # ingredient = input('Enter an ingredient: ')  - Commented out by Janet
+    results = recipe_search(ingredient, max_time, cuisine, health)
 
     if results != []:             # IF search result is not empty, create the table
         for item in results:
             recipe = item['recipe']
             label = recipe['label']
-            cuisine = recipe['cuisineType']
+            cuisine = str(recipe['cuisineType'])  # convert to string - Alex
+            allowed_chars = set('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ ,')
+            # set allowed characters (we want to keep letters + commas) - Alex
+            cuisine = ''.join(filter(allowed_chars.__contains__, cuisine))
+            # extract only allowed characters, as set above - Alex
 
             calories = round(recipe['calories'])        # rounded to nearest integer - Janet
             carbs = round(recipe['totalNutrients']['CHOCDF']['quantity'])  # grams of carbohydrates
@@ -107,7 +118,7 @@ def run_csv(ingredient, max_time, cuisine):
             # total time to prep and cook recipe - not foolproof as depends on original recipe listing
             time = round(recipe['totalTime'])
             link = recipe['url']
-            webbrowser.open(link)  # automatically open web browser tabs for each recipe
+            # webbrowser.open(link)  # automatically open web browser tabs for each recipe
             # disable during development :)
 
             d.append(
@@ -115,6 +126,7 @@ def run_csv(ingredient, max_time, cuisine):
                     'Search term': ingredient,
                     'Recipe name': label,
                     'Cuisine Type': cuisine,
+                    'Diet type': health,
                     'Total time': time,
                     'Calories': calories,
                     'CHO': carbs,
@@ -125,8 +137,12 @@ def run_csv(ingredient, max_time, cuisine):
             )
         recipe_df = pd.DataFrame(d)
         recipe_df.to_csv('recipe-search.csv', index=False)
-        apples_recipes = pd.read_csv('recipe-search.csv')       # Read the csv file,
-        apples_recipes.to_html('./templates/recipes.html')      # then convert to a recipes.HTML page - Janet
+        apples_recipes = pd.read_csv('recipe-search.csv')       # Read the csv file
+        apples_recipes['URL'] = '<a href=' + apples_recipes['URL'] + '><div>' + apples_recipes['URL'] + '</div></a>'
+        # Set hyperlink - Alex
+        apples_recipes.to_html('./templates/recipes.html', escape=False)
+        # then convert to a recipes.HTML page - Janet
+        # escape=False needed for hyperlink to render - Alex
 
     else:
         print('No search results!')
